7ba750018194d94a2e91caea561776e6db53a863
Gurvir Dhillon <72519948+gurvirdhillon@users.noreply.github.com>
2026-04-05 12:21:29 +0100

    deloaded planning and profile page

---
+++ src/handle_btn.js
@@ -0,0 +1,49 @@
+const getButton = document.querySelector("#log_btn")
+getButton.addEventListener("click", () => {
+    window.location.href = "lift.html"
+});
+
+
+const workoutData = {
+    user_id: document.querySelector('[name="user_id"]').value,
+    date: document.querySelector('[name="date"]').value,
+    exercise: document.querySelector('[name="exercise"]').value,
+    sets: document.querySelector('[name="sets"]').value,
+    reps: document.querySelector('[name="reps"]').value,
+    weight_kg: document.querySelector('[name="weight_kg"]').value,
+    session_duration_hr: document.querySelector('[name="session_duration_hr"]').value,
+    calories_burned: document.querySelector('[name="calories_burned"]').value,
+    avg_bpm: document.querySelector('[name="avg_bpm"]').value,
+    max_bpm: document.querySelector('[name="max_bpm"]').value,
+    water_intake_l: document.querySelector('[name="water_intake_l"]').value,
+    workout_type: document.querySelector('[name="workout_type"]').value,
+    workout_difficulty: document.querySelector('[name="workout_difficulty"]').value,
+    workout_category: document.querySelector('[name="workout_category"]').value
+  };
+
+console.log(workoutData)
+
+const getButton = document.querySelector("#log_btn")
+getButton.addEventListener("click", () => {
+    window.location.href = "lift.html"
+});
+
+
+const workoutData = {
+    user_id: document.querySelector('[name="user_id"]').value,
+    date: document.querySelector('[name="date"]').value,
+    exercise: document.querySelector('[name="exercise"]').value,
+    sets: document.querySelector('[name="sets"]').value,
+    reps: document.querySelector('[name="reps"]').value,
+    weight_kg: document.querySelector('[name="weight_kg"]').value,
+    session_duration_hr: document.querySelector('[name="session_duration_hr"]').value,
+    calories_burned: document.querySelector('[name="calories_burned"]').value,
+    avg_bpm: document.querySelector('[name="avg_bpm"]').value,
+    max_bpm: document.querySelector('[name="max_bpm"]').value,
+    water_intake_l: document.querySelector('[name="water_intake_l"]').value,
+    workout_type: document.querySelector('[name="workout_type"]').value,
+    workout_difficulty: document.querySelector('[name="workout_difficulty"]').value,
+    workout_category: document.querySelector('[name="workout_category"]').value
+  };
+
+console.log(workoutData)
